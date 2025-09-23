f5e85a890436d8afaca50f001696879eb9a8d1c7
Victor Avellar <victor.avellar@sonda.com>
2025-09-23 20:18:33 -0300

    feat: getting data from residents and vehicles

--- src/app/planets/[id]/page.tsx
+++ src/app/planets/[id]/page.tsx
@@ -1,16 +1,17 @@
 import {Suspense} from "react";
-import {PlanetDetail} from "@/app/planets/components/PlanetDetail";
 import Link from "next/link";
 import {PlanetCardSkeleton} from "@/components/PlanetCardSkeleton";
+import PlanetDetail from "@/app/planets/components/PlanetDetail";
 
 export default async function PlanetPage({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
+    console.log(id)
     return (
         <div>
            <div className="text-center mt-4 ">
                <h1 className="text-4xl">Planet details: </h1>
            </div>
-            <Suspense fallback={<PlanetCardSkeleton/>}>
+            <Suspense fallback={<h2 className="text-center my-4 ">Search planet details...</h2>}>
                <PlanetDetail id={id}/>
             </Suspense>
             <div className="text-center mt-4 ">
--- src/components/PlanetList/index.tsx
+++ src/components/PlanetList/index.tsx
@@ -41,7 +41,7 @@ export function PlanetList({ planets, nextUrl, prevUrl, isLoading, onFetchPage }
         <div className="grid gap-4 w-full">
             <div className="flex mt-8 w-full justify-between flex-col sm:flex-row">
                 <button
-                    className="px-4 py-2 bg-gray-600 text-white rounded disabled:opacity-50 cursor-pointer"
+                    className="px-4 py-2 bg-gray-600 text-white rounded disabled:opacity-50 cursor-pointer mb-2 sm:mb-0"
                     onClick={() => prevUrl && onFetchPage(prevUrl)}
                     disabled={!prevUrl}
                 >
